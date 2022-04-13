4eaaae1d083b020c4e22e8c4fa808c300e8f0cab
Martin Farkas <42645825+BlackLight54@users.noreply.github.com>
2022-04-13 20:46:49 +0200

    Cleared up data transfer to container

--- load_testing/system_setup.sh
+++ load_testing/system_setup.sh
@@ -9,10 +9,12 @@ threadCount=5
 cd "$(dirname "$0")"
 mkdir -p dump &&
 rm -f ./dump/*  &&
-./system_launch.sh -r -b &&
+./system_launch.sh -r &&
 docker exec opencbdc-tx_client ./load_testing/gen_test_env.sh -w $wallet_csv -t $transaction_csv -n $numOfTxs -c $numOfWallets && 
-docker cp  opencbdc-tx_client:/opt/tx-processor/load_testing/wallets.csv ./dump/ &&
-docker cp  opencbdc-tx_client:/opt/tx-processor/load_testing/transactions.csv ./dump/ &&
+docker cp  opencbdc-tx_client:/opt/tx-processor/load_testing/wallets.csv dump/ &&
+docker cp  opencbdc-tx_client:/opt/tx-processor/load_testing/transactions.csv dump/ &&
+echo "Exported test dataset to dump/"
+echo "Starting Jmeter test with $threadCount threads on $numOfWallets wallets and $numOfTxs txs" &&
 docker exec opencbdc-tx_client /opt/apache-jmeter-5.4.3/bin/jmeter -t ./load_testing/TransactionTest_remote.jmx -l ./load_testing/dump/results.jtl -j ./load_testing/dump/jmeter.log -n -DtxCount=$numOfTxs -Dthreadcount=$threadCount &&
 docker cp opencbdc-tx_client:/opt/tx-processor/load_testing/dump/ . &&
-echo "Exported results to ./dump"
+echo "Exported results to dump/"
