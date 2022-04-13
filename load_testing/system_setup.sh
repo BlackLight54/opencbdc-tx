e0b6d49f554ea4958a5c9feac9574c85c5ccd1f8
Martin Farkas <42645825+BlackLight54@users.noreply.github.com>
2022-04-13 21:35:42 +0200

    Fixed command dependency

--- load_testing/system_setup.sh
+++ load_testing/system_setup.sh
@@ -13,7 +13,7 @@ rm -f ./dump/*  &&
 docker exec opencbdc-tx_client ./load_testing/gen_test_env.sh -w $wallet_csv -t $transaction_csv -n $numOfTxs -c $numOfWallets && 
 docker cp  opencbdc-tx_client:/opt/tx-processor/load_testing/wallets.csv dump/ &&
 docker cp  opencbdc-tx_client:/opt/tx-processor/load_testing/transactions.csv dump/ &&
-echo "Exported test dataset to dump/"
+echo "Exported test dataset to dump/" &&
 echo "Starting Jmeter test with $threadCount threads on $numOfWallets wallets and $numOfTxs txs" &&
 docker exec opencbdc-tx_client /opt/apache-jmeter-5.4.3/bin/jmeter -t ./load_testing/TransactionTest_remote.jmx -l ./load_testing/dump/results.jtl -j ./load_testing/dump/jmeter.log -n -DtxCount=$numOfTxs -Dthreadcount=$threadCount &&
 docker cp opencbdc-tx_client:/opt/tx-processor/load_testing/dump/ . &&
